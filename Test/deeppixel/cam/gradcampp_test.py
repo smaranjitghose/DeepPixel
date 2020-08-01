64a8ab6ddc5ec30c534041e4dc8f96fbab6f1763
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-08-02 03:47:13 +0530

    Added Gradcam++

---
+++ Test/deeppixel/cam/gradcampp_test.py
@@ -0,0 +1,16 @@
+import torchvision.models as models
+
+from deeppixel.cam import *
+
+
+image_path = 'Test/deeppixel/cam/bull_mastiff.png'
+#image_name = image_path.split('/')[1]
+image = load_image(image_path)
+norm_image = apply_transforms(image)
+
+model = models.vgg16(pretrained=True)
+model_dict = dict(arch=model, layer_name='features_29', input_size=(224, 224))
+
+gradcampp = GradCAMpp(model_dict)
+output = gradcampp(norm_image)
+visualize(norm_image, output)
